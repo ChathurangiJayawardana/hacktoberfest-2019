45216ebc1ecfa6adb9f4c7b38b479841f3da6ae5
Chathurangi Jayawardana <Chathurangi@local.dev7>
2019-10-02 13:35:59 +0530

    Commit message

--- scripts/java_helloworld_chathurangijayawardana.java
+++ scripts/java_helloworld_chathurangijayawardana.java
@@ -4,7 +4,7 @@
 // GITHUB:https://github.com/ChathurangiJayawardana
 
 public class HelloWorld {
-  public static void main() {
+    public static void main(String[] args) {
     System.out.println("Hello, World!");
   }
 }
